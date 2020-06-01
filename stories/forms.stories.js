09752801c11d8240e6416909261ae3d0a27bec50
James Lyons <jamesjlyons29@gmail.com>
2020-05-31 17:19:41 -0700

    add block and large text inputs

--- stories/forms.stories.js
+++ stories/forms.stories.js
@@ -6,3 +6,11 @@ export default {
 
 export const Text = () => `<label>First name</label><br />
       <input type="text" name="first_name" />`;
+
+export const TextLarge = () => `<label>First name</label><br />
+      <input type="text" name="first_name" class="large" />`;
+
+export const TextBlock = () => `<label>First name</label><br />
+      <input type="text" name="first_name" class="block" />`;
+export const TextBlockLarge = () => `<label>First name</label><br />
+      <input type="text" name="first_name" class="large block" />`;
